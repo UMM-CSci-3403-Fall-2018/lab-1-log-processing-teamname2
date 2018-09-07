0694bfbc181341bf7f5d46fedee2391e7be52780
bella148 <bella148@morris.umn.edu>
2018-09-07 14:15:40 -0500

    Fix grep searching and awk extraction

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -3,5 +3,12 @@
 # Go to the directory specified
 cd $1
 
-# Create a file for the skimmed logs
-grep -wr "(^[A-Z]\w+\s\s\d+\s\d+)" --exclude=failed_login_data.txt > failed_login_data.txt
+# Extract all the relivant lines in our two cases
+grep -wr "Failed password for" --exclude=failed_login_data.txt | awk '$9 != "invalid" {print $1, $2, substr($3, 1, 2), $9, $11}' | cat >> failed_login_data.txt
+grep -wr "Failed password for" --exclude=failed_login_data.txt | awk '$9 == "invalid" {print $1, $2, substr($3, 1, 2), $11, $13}' | cat >> failed_login_data.txt
+
+# Clean up 'secure-*' text
+awk -F: '{print $2}' failed_login_data.txt
+
+
+
