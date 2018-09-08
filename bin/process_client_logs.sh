e257655d5333543bc47b85a493f7c02f806aa251
Blake <bella148@morris.umn.edu>
2018-09-08 15:46:35 -0700

    Finish process_client_log and add comment

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -3,12 +3,15 @@
 # Go to the directory specified
 cd $1
 
-# Extract all the relivant lines in our two cases
-grep -wr "Failed password for" --exclude=failed_login_data.txt | awk '$9 != "invalid" {print $1, $2, substr($3, 1, 2), $9, $11}' | cat >> failed_login_data.txt
-grep -wr "Failed password for" --exclude=failed_login_data.txt | awk '$9 == "invalid" {print $1, $2, substr($3, 1, 2), $11, $13}' | cat >> failed_login_data.txt
-
-# Clean up 'secure-*' text
-awk -F: '{print $2}' failed_login_data.txt
+# This is all one line, but adjusted for easier viewing
+# The first command, grep, will look for all of the lines in each secure file that is relivant to us
+# The second command, awk, checks to see if the user is invalid or not, and record the results accordingly
+# The third command, awk, will remove the secure-* before the text
+# The fourth command, cat, will print the outcome to a file
+grep -wr "Failed password for" --exclude=failed_login_data.txt | 
+awk '{if($9 == "invalid"){print $1, $2, substr($3, 1, 2), $11, $13}else{print $1, $2, substr($3, 1, 2), $9, $11}}' | 
+awk -F: '{print $2}' | 
+cat >> failed_login_data.txt
 
 
 
