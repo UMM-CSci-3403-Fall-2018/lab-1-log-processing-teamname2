d74aebe7ba077d757429a6f08d44fbf22c9e8ab0
john9hoff <hoffx247@morris.umn.edu>
2018-09-10 19:47:19 -0500

    Integrate wrap_contents.sh with create_hours_dist.sh and rename content filename in create_username_dist.sh

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-find $1 -name "*failed_login_data*" -exec awk '{print $4}' {} + | sort | uniq -c | awk '{print "data.addRow([\x27"$2"\x27, "$1"]);"}' | cat >> $1/content.html
+find $1 -name "*failed_login_data*" -exec awk '{print $4}' {} + | sort | uniq -c | awk '{print "data.addRow([\x27"$2"\x27, "$1"]);"}' | cat >> $1/usernames_content.html
 
-bin/wrap_contents.sh $1/content.html html_components/username_dist $1/username_dist.html
+bin/wrap_contents.sh $1/usernames_content.html html_components/username_dist $1/username_dist.html
