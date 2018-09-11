6bb92c4558edeaca5609cf1fa9b1b6066ec247ad
john9hoff <hoffx247@morris.umn.edu>
2018-09-10 19:46:59 -0500

    Integrate wrap_contents.sh with create_hours_dist.sh and rename content filename in create_username_dist.sh

--- bin/create_hours_dist.sh
+++ bin/create_hours_dist.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
+find $1 -name "*failed_login_data*" -exec awk '{print $3}' {} + | sort | uniq -c | awk '{print "data.addRow([\x27"$2"\x27, "$1"]);"}' | cat >> $1/hours_content.html
 
-# Add the header (Will change to use wrap_contents.sh in the future
-cat html_components/hours_dist_header.html >> $1/hours_dist.html
-
-# This is all one line, here is a breakdown
-# find looks into all the subdirectories of the given directory and looks for "failed_login_data.txt"
-# awk is passed the results of find and prints the fourth column, which is the user names
-# sort is passed the results of awk and sorts the results alphabeticlly
-# uniq then counts the occurances of the names
-# awk then constructs a string that will be used to add data to our chart
-# cat finally prints all of it into username_dist.html
-find $1 -name "*failed_login_data*" -exec awk '{print $3}' {} + | sort | uniq -c | awk '{print "data.addRow([\x27"$2"\x27, "$1"]);"}' | cat >> $1/hours_dist.html
-
-# Add the footer
-cat html_components/hours_dist_footer.html >> $1/hours_dist.html
+bin/wrap_contents.sh $1/hours_content.html html_components/hours_dist $1/hours_dist.html
