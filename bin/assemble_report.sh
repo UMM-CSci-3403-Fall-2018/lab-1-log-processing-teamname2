8c72d27e426bf7d723054aa5174d730bd657a46a
Blake <bella148@morris.umn.edu>
2018-09-18 11:34:26 -0700

    Finish log_process.sh and include more comments across scripts

--- bin/assemble_report.sh
+++ bin/assemble_report.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+# Store the directory in a variable
+DIR=$1
+
 # Take each of the graphs generated from previous scripts and put them into a temporary file
-cat $1/country_dist.html >> $1/failed_login_summary_contents.html
-cat $1/hours_dist.html >> $1/failed_login_summary_contents.html
-cat $1/username_dist.html >> $1/failed_login_summary_contents.html
+cat $DIR/country_dist.html >> $DIR/failed_login_summary_contents.html
+cat $DIR/hours_dist.html >> $DIR/failed_login_summary_contents.html
+cat $DIR/username_dist.html >> $DIR/failed_login_summary_contents.html
 
 # Use the script wrap_contents to add a header and a footer to the data created above to produce failed_login_summary.html
-bin/wrap_contents.sh $1/failed_login_summary_contents.html html_components/summary_plots $1/failed_login_summary.html
+bin/wrap_contents.sh $DIR/failed_login_summary_contents.html html_components/summary_plots $DIR/failed_login_summary.html
 
 # Remove temp file failed_login_summary_contents.html
-rm $1/failed_login_summary_contents.html
+rm $DIR/failed_login_summary_contents.html
